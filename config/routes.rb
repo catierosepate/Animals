8e6a1ecb02d17b0606465459bf31dc9c1473c627
catierosepate <cpate@lsoc.org>
2014-03-19 14:39:09 +0000

    change to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Animals::Application.routes.draw do
   resources :animals
 
+  #change
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

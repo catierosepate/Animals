b8e92ce9a9d79e97422a21a767baeab65617e691
catierosepate <cpate@lsoc.org>
2014-03-19 14:33:31 +0000

    first commit

---
+++ app/views/animals/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @animal, :id, :name, :image_url, :description, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Animals::Application.config.secret_key_base = '62ec9cdb11474906eb26c8499b65991234a95c117ff48cf65ec794a61ec05438f95e355649f71db6a1ae5fb1436f34e569c2c52fa0c4d5bad95ffcaf065c147b'
